a05282f815a085387177ca8f5e609d31a35d39f1
puzzlepiece87 <30557512+puzzlepiece87@users.noreply.github.com>
2025-09-11 20:13:36 -0500

    Add files via upload

---
+++ CheevoFight/CheevoFight/ViewPlusViewModel/Windows/ProgressBar.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace CheevoFight.ViewPlusViewModel.Windows
+{
+    public partial class ProgressBar : Window
+    {
+        public ProgressBar(ViewModel viewModel)
+        {
+            InitializeComponent();
+            this.DataContext = viewModel;
+        }
+    }
+}
