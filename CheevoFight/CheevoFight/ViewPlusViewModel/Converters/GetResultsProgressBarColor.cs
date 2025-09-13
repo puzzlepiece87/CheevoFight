cd5851ad5b4b2f10a50dc2fe12d8008716cd5197
puzzlepiece87 <30557512+puzzlepiece87@users.noreply.github.com>
2025-09-13 00:05:09 -0500

    Add files via upload
    20250913 Program now accepts standard Steam profile URLs (before it only accepted vanity URLs)

---
+++ CheevoFight/CheevoFight/ViewPlusViewModel/Converters/GetResultsProgressBarColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CheevoFight.ViewPlusViewModel.Converters
+{
+    public class GetResultsProgressBarColor : DependencyObject, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ToInt32(value) >= 50 ? Brushes.Blue : Brushes.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
